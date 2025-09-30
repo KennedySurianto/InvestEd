01aa24e3091e7f53bbb713e34d900cc0c2da085b
Kennedy Surianto <kennedysrnt@gmail.com>
2025-09-30 15:57:49 +0700

    add: frontend starting files | update: start.bat to open be and fe

--- start.bat
+++ start.bat
@@ -1,4 +1,13 @@
 @echo off
-ECHO Starting the development server...
-cd backend
-nodemon index.js
+ECHO Starting the development server in Windows Terminal tabs...
+
+:: Save current directory
+set CURRENT_DIR=%cd%
+
+:: Open backend in new CMD tab
+wt -w 0 nt -p "Command Prompt" -d "%CURRENT_DIR%\backend" cmd /k "nodemon index.js"
+
+:: Open frontend in new CMD tab
+wt -w 0 nt -p "Command Prompt" -d "%CURRENT_DIR%\frontend" cmd /k "npm run dev"
+
+ECHO Both backend and frontend are running in new CMD tabs.
